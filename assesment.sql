612175bc4b2d2e0702da0126045f913d32ff1ed5
Mreegank Sinha <mreeganksinha76@gmail.com>
2025-04-03 18:16:52 +0530

    Add files via upload

---
+++ assesment.sql
@@ -0,0 +1,41 @@
+select * from assesment;
+
+select price from assesment;
+
+select price from assesment 
+order by 1 asc;
+
+delete from assesment where price = 0;
+
+select price from assesment; 
+
+select status from assesment;
+
+---select * from assesment where status = 'Pending';
+
+---select * from assesment where status = 'Confirmed';
+
+select price from assesment;
+
+select price from assesment
+order by 1 asc;
+
+delete from assesment where price = 0;
+
+select price from assesment
+order by 1 asc;
+
+select subscriptiontype from assesment;
+
+select subscriptiontype from assesment
+order by 1 desc;
+
+alter table assesment drop column subscriptiontype;
+
+select * from assesment;
+
+--SELECT * FROM assesment WHERE theme IS NOT NULL;
+
+--SELECT * FROM assesment WHERE instructor IS NOT NULL;
+
+--SELECT * FROM assesment WHERE classtype IS NOT NULL;
